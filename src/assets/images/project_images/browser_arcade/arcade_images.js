84c8d3c8c63c29ca2bc11d6e3aa1714df5891bd9
Katie Loesch <catherine.loesch@protonmail.com>
2024-08-20 19:31:45 +0100

    change order of images in array

--- src/assets/images/project_images/browser_arcade/arcade_images.js
+++ src/assets/images/project_images/browser_arcade/arcade_images.js
@@ -1,6 +1,5 @@
 import img1 from './ttt_1.png';
 import img2 from './ttt_2.png';
-import img3 from './ttt_3.png';
 import img4 from './ttt_4.png';
 import img5 from './ttt_5.png';
 import img6 from './ttt_6.png';
@@ -13,25 +12,25 @@ import img11 from './ttt_11.png';
 const arcadeImages = [
   {
     id: 0,
-    src: img1,
-    title: 'landing page',
-    alt: 'screenshot of landing page',
-    type: 'project-screenshot',
-  },
-  {
-    id: 1,
     src: img2,
     title: 'game in progress',
     alt: 'screenshot of game in progress',
     type: 'project-screenshot',
   },
   {
-    id: 2,
-    src: img3,
-    title: 'add player name form',
-    alt: 'screenshot of form to add player name',
+    id: 1,
+    src: img1,
+    title: 'landing page',
+    alt: 'screenshot of landing page',
     type: 'project-screenshot',
   },
+  // {
+  //   id: 2,
+  //   src: img3,
+  //   title: 'add player name form',
+  //   alt: 'screenshot of form to add player name',
+  //   type: 'project-screenshot',
+  // },
   {
     id: 3,
     src: img4,
--- src/assets/images/project_images/guestbook/guestbook_images.js
+++ src/assets/images/project_images/guestbook/guestbook_images.js
@@ -7,16 +7,16 @@ import img5 from './guestbook_5.png';
 const guestbookImages = [
   {
     id: 0,
-    src: img1,
-    title: 'mern guestbook homepage',
-    alt: 'screenshot of mern guestbook homepage',
+    src: img2,
+    title: 'mern guestbook posts page',
+    alt: 'screenshot of mern guestbook posts page',
     type: 'project-screenshot',
   },
   {
     id: 1,
-    src: img2,
-    title: 'mern guestbook posts page',
-    alt: 'screenshot of mern guestbook posts page',
+    src: img1,
+    title: 'mern guestbook homepage',
+    alt: 'screenshot of mern guestbook homepage',
     type: 'project-screenshot',
   },
   {
