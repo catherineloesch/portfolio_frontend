0d1ac54fa48633f01367446b5d6ba212426cefae
Katie Loesch <catherine.loesch@protonmail.com>
2024-08-20 19:31:45 +0100

    refactor project descriptions into separate file

--- src/api_data/api_projects.js
+++ src/api_data/api_projects.js
@@ -1,4 +1,5 @@
-import { projectImages } from '../assets/images/project_images';
+import descriptions from './api_project_descriptions';
+import projectImages from '../assets/images/project_images/project_images';
 import tech from './api_technologies';
 
 const projects = [
@@ -25,9 +26,7 @@ const projects = [
   {
     id: 2,
     title: 'Pawnee Pets',
-    description: [
-      'A fully responsive full-stack job-board application, built from scratch in 10 days using a rails API as its back end and a react-based front end. Featuring freelance jobs posted by pet owners, the application enables users to make an account and post job listings looking for dog-walkers or pet sitters. Once logged in they can view their listings as well as edit and delete them. They can also update their account information and delete their account.',
-    ],
+    description: descriptions.jobBoard,
     projectLink: 'http://pawnee-pets.katieloesch.co.uk',
     codeLink: 'https://github.com/katieloesch/project4-react-frontend',
     imgArr: projectImages.jobBoard,
@@ -46,9 +45,7 @@ const projects = [
   {
     id: 3,
     title: 'Film & TV Stuff',
-    description: [
-      "A movie and tv watchlist SPA with full CRUD functionality built from scratch in 8 days using ReactJS/SCSS. The user can add movies and tv shows to a 'watched' and a 'to-watch' list, mark them as watched as well as edit and remove them from the list. By clicking on cover art of a film and or tv show, more information is displayed about the title. The user also has the option to add movies and tv shows from the public TMDB API to their lists and modify/remove them. The updated version of the project is fully responsive for easy navigation on all screen sizes and connects to firestore/firebase allowing users to create an account and save their data.",
-    ],
+    description: descriptions.flicks4brains,
     projectLink: 'http://film-tv-stuff.katieloesch.co.uk',
     codeLink:
       'http://github.com/katieloesch/project-2-react-film-app-revisited',
@@ -65,11 +62,7 @@ const projects = [
   {
     id: 4,
     title: 'MERN Guestbook',
-    description: [
-      [
-        'A Full-Stack Guestbook application, built from scratch within 9 days by a team of 4 developers using the MERN stack. The website allows users to sign up for an account and subsequently log into and out of their account. They also can also edit their username and password once they are logged in, or delete their account. Authentication was achieved by using the Node ExpressJS and Passport.js packages. Users can also read, post, edit and delete comments in the Guestbook application, the concept of which was tailored around a specific event type: a divorce party. The idea behind this concept was to support recent divorcees by enjoying their divorce party to the fullest and later re-live all the happy memories.',
-      ],
-    ],
+    description: descriptions.guestbook,
     projectLink: 'https://mern-guestbook.katieloesch.co.uk/',
     codeLink: 'https://github.com/katieloesch/p3-mern-guestbook-frontend',
     imgArr: projectImages.guestbook,
@@ -91,9 +84,7 @@ const projects = [
   {
     id: 5,
     title: 'Todo App',
-    description: [
-      "A todo list application built from scratch using React.js with full CRUD functionality: Users can add new tasks to the list, mark tasks as completed, edit tasks and remove tasks from the list. The application also displays the total number of tasks remaining and tasks are saved in the browser's local storage, so they persist after reloading the page. There is also filtering functionality to show completed tasks only, incomplete tasks or all tasks. Application state is managed with Redux and styling was achieved using CSS modules with a fully responsive design.",
-    ],
+    description: descriptions.pomodoro,
     projectLink: 'http://todo-app.katieloesch.co.uk/',
     codeLink: 'https://github.com/katieloesch/react-todo-app',
     imgArr: projectImages.pomodoro,
@@ -103,9 +94,7 @@ const projects = [
   {
     id: 6,
     title: 'Tic Tac Toe',
-    description: [
-      "A classic game of Tic Tac Toe built from scratch in 7 days using HTML/CSS/JS. This dynamic game allows two players to compete from the same computer. It lets users enter their name and pick a colour. After they start the game, a preview of their symbol will appear in any empty cell when they hover over it and a display notifies them about whose turn it is, who wins or if there's a draw. Users can also reset the game and keep track of the number of draws and wins. The game features animations and sound effects, which can be muted and the LocalStorage option enables users to continue their gaming session where they left off after the page reloads or the internet connection is interrupted.",
-    ],
+    description: descriptions.browserArcade,
     projectLink: 'http://tictactoe.katieloesch.co.uk/',
     codeLink: 'http://github.com/katieloesch/project-1-tictactoe',
     imgArr: projectImages.arcade,
